07a586aae2780c05d2764c85ae9597b9fa0166dd
edu <edusanc95@gmail.com>
2016-10-25 18:50:17 +0200

    Fixed some bugs.

--- Puzzle/src/main.cpp
+++ Puzzle/src/main.cpp
@@ -9,14 +9,13 @@ using namespace std;
 using namespace cimg_library;
 
 //This is the comparation function needed for sort() for vectors
-bool compareFunction(Tile i, Tile j){
-    return (i.getIdentifier()<j.getIdentifier());
+bool compareFunction(Tile i, Tile j) {
+	return (i.getIdentifier() < j.getIdentifier());
 }
-
 
 //This method crops the Image and stores it in a custom object of type Matrix, which simulates an array, called tileArray.
 void cropImage(int cols, int rows, int x, int y, CImg<unsigned char> ogImage,
-		auto &tileArray) {
+		Matrix<Tile, 10, 10> &tileArray) {
 
 	int id = 1; //Identifier for the different images
 	for (int i = 0; i < cols; i++) {
@@ -24,15 +23,25 @@ void cropImage(int cols, int rows, int x, int y, CImg<unsigned char> ogImage,
 
 			//We create the tile related with the cropped image.
 			//Tile tile(id, ogImage.get_crop(x * j + x, y * i + y, x * j, y * i));
-			Tile tile(id, ogImage.get_crop(((x * j + x)-1), ((y * i + y)-1), x * j, y * i));
+			/*if(id=1){
+			 CImg<unsigned char> aux (x, y, 1, 3, 0);
+			 Tile tile(id, aux);
+			 tileArray(i, j) = tile;
+			 id++;
+			 }else{*/
+			Tile tile(id,
+					ogImage.get_crop(((x * j + x) - 1), ((y * i + y) - 1),
+							x * j, y * i));
 			tileArray(i, j) = tile;
 			id++;
+			//}
 		}
 	}
 }
 
 //This method shows in a window the different tiles of the image, with the corresponding adjacent tiles in the standard output
-void showTiles(int cols, int rows, auto tileArray, CImgDisplay *draw_disp) {
+void showTiles(int cols, int rows, Matrix<Tile, 10, 10> tileArray,
+		CImgDisplay *draw_disp) {
 	std::list<Tile>* ady = new list<Tile>(4); //To store the adyacents
 	CImg<unsigned char> auxImage("puzzle.png");
 	for (int z = 0; z < cols; z++) {
@@ -53,17 +62,19 @@ void showTiles(int cols, int rows, auto tileArray, CImgDisplay *draw_disp) {
 
 //This method reconstructs the Image from the Matrix of Tiles given and returns it.
 CImg<unsigned char> reconstructImage(int cols, int rows, int x, int y,
-		CImg<unsigned char> ogImage, auto tileArray) {
+		CImg<unsigned char> ogImage, Matrix<Tile, 10, 10> tileArray) {
+	ogImage.fill(255, 0, 0);
 	for (int q = 0; q < cols; q++) {
 		for (int s = 0; s < rows; s++) {
 			//This is used when we don't have an initial black tile
-			/*if(tileArray(q, s).getIdentifier()==1){
-			 CImg<unsigned char> aux(tileArray(q, s).getImage().width(),tileArray(q, s).getImage().height(),1,3,0);
-			 ogImage.draw_image(s * x, q * y, aux);
-			 }else{
-			 ogImage.draw_image(s * x, q * y, tileArray(q, s).getImage());
-			 }*/
-			ogImage.draw_image(s * x, q * y, tileArray(q, s).getImage());
+			if (tileArray(q, s).getIdentifier() == 1) {
+				CImg<unsigned char> aux(tileArray(q, s).getImage().width(),
+						tileArray(q, s).getImage().height(), 1, 3, 0);
+				ogImage.draw_image(s * x, q * y, aux);
+			} else {
+				ogImage.draw_image(s * x, q * y, tileArray(q, s).getImage());
+			}
+			//ogImage.draw_image(s * x, q * y, tileArray(q, s).getImage());
 		}
 	}
 
@@ -71,14 +82,17 @@ CImg<unsigned char> reconstructImage(int cols, int rows, int x, int y,
 }
 
 //This method changes the identifiers of the tiles of the messy array to the ones of the og image so we can reconstruct it.
-void solvePuzzle(int cols, int rows, auto &tileArray, auto &messyArray) {
+void solvePuzzle(int cols, int rows, Matrix<Tile, 10, 10> &tileArray,
+		Matrix<Tile, 10, 10> &messyArray) {
 	for (int q = 0; q < cols; q++) {
 		for (int s = 0; s < rows; s++) {
 
 			for (int i = 0; i < cols; i++) {
 				for (int j = 0; j < rows; j++) {
-					if (messyArray(s, q).getImage() == tileArray(j, i).getImage()) {
-						messyArray(s, q).setIdentifier(tileArray(j, i).getIdentifier());
+					if (messyArray(q, s).getImage()
+							== tileArray(i, j).getImage()) {
+						messyArray(q, s).setIdentifier(
+								tileArray(i, j).getIdentifier());
 
 					}
 				}
@@ -89,34 +103,38 @@ void solvePuzzle(int cols, int rows, auto &tileArray, auto &messyArray) {
 
 	//Now we get all the data from the matrix into a vector.
 	std::vector<Tile> vect;
-	for (int i = 0; i < cols; i++){
-		for (int j = 0; j < rows; j++){
-			vect.push_back(tileArray(j, i));
+	for (int i = 0; i < cols; i++) {
+		for (int j = 0; j < rows; j++) {
+			vect.push_back(tileArray(i, j));
 		}
 	}
 
 	//and sort the vector using standart sort() function
 	std::sort(vect.begin(), vect.end(), compareFunction);
 
-	//Finally, we put the data back into the matrix
-	for (int i = 0; i < cols; i++){
-	        for (int j = 0; j < rows; j++){
-	            messyArray(j,i) = vect.at(j*rows + i);
 
-	        }
+	std::cout << "myvector contains:";
+	for (std::vector<Tile>::iterator it = vect.begin(); it != vect.end();
+			++it)
+		std::cout << ' ' << it->getIdentifier();
+	std::cout << '\n';
+
+	//Finally, we put the data back into the matrix
+	for (int i = 0; i < cols; i++) {
+		for (int j = 0; j < rows; j++) {
+			messyArray(i, j) = vect.at(i * rows + j);
+		}
 	}
 }
 
-template <typename T>
+template<typename T>
 T cinitialize(std::istream & is) noexcept
 {
-    T x;
-    return (is && is >> x) ? x : T();
+	T x;
+	return (is && is >> x) ? x : T();
 }
 
-
 int main(int argc, char **argv) {
-
 
 	int cols, rows;
 	string puzzle;
