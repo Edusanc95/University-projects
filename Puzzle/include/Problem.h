f99e1e4b593c568c10e3e5c90e3ae17c34e585dd
edu <edusanc95@gmail.com>
2016-11-28 03:35:49 +0100

    Testing started.

---
+++ Puzzle/include/Problem.h
@@ -0,0 +1,39 @@
+/*
+ * Problem.h
+ *
+ *  Created on: Nov 25, 2016
+ *      Author: edu
+ */
+
+#ifndef INCLUDE_PROBLEM_H_
+#define INCLUDE_PROBLEM_H_
+#include "state.h"
+#include "stateSpace.h"
+#include "node.h"
+#include "frontier.h"
+#include "Sucessor.h"
+#include "CImg.h"
+#include "ImageManipulation.h"
+
+class Problem{
+public:
+	Problem(state initialState, stateSpace aStateSpace);
+	~Problem();
+
+	bool boundedSearch(string strategy, int max_Depth);
+	bool Search();
+
+private:
+	state _initialState;
+	stateSpace _stateSpace;
+
+	list<node*> createListTreeNodes(list<Sucessor>* sucessors, node* pNode, int max_Depth, string strategy);
+
+	//For graphical representation of what's happening
+
+	ImageManipulation _ImageManipulator;
+	CImgDisplay _reconstructing_disp;
+};
+
+
+#endif /* INCLUDE_PROBLEM_H_ */
---
+++ Puzzle/src/Problem.cpp
@@ -0,0 +1,82 @@
+/*
+ * Problem.cpp
+ *
+ *  Created on: Nov 25, 2016
+ *      Author: edu
+ */
+#include "Problem.h"
+
+Problem::Problem(state initialState, stateSpace aStateSpace): _initialState(initialState), _stateSpace(aStateSpace){
+
+}
+
+Problem::~Problem(){
+
+}
+
+//change list<>* for list<*>
+bool Problem::boundedSearch(string strategy, int max_Depth){
+	//Initialization process
+	CImg<unsigned char> auxImage("puzzle.png");
+	Frontier* pFrontier = new Frontier();
+	node* n_actual;
+
+	node* n_initial = new node(NULL, _initialState, 0, "nothing", 0, 0);
+	pFrontier->insert(n_initial);
+	bool isSolution = false;
+
+	int maxAux = 100;
+	int iterator = 0;
+
+	//Search loop
+	//Remember to eliminate iterator and maxAux.
+	while(!isSolution && !pFrontier->isEmpty() && iterator < maxAux){
+		n_actual = pFrontier->removeFirst();
+		cout << n_actual->getAction()<< endl;
+		_ImageManipulator.showIds(n_actual->getState().getCols(), n_actual->getState().getRows(), *(n_actual->getState().getTileArray()));
+		//Representation of what's happening on screen, can be erased without problems.
+		auxImage =_ImageManipulator.reconstructImage(n_actual->getState().getCols()
+				   ,n_actual->getState().getRows()
+				   ,auxImage.width() / n_actual->getState().getRows()
+				   ,auxImage.height() / n_actual->getState().getCols()
+				   ,auxImage
+				   ,*(n_actual->getState().getTileArray()));
+		_reconstructing_disp.display(auxImage);
+
+		sleep(1);
+
+		if(_stateSpace.isGoal(n_actual->getState())){
+			isSolution = true;
+		}else if(iterator < maxAux){
+			list<Sucessor>* sucessors = _stateSpace.Successor(n_actual->getState());
+			list<node*> nodes;
+			nodes = createListTreeNodes(sucessors, n_actual, max_Depth, strategy);
+			for (std::list<node*>::iterator it=nodes.begin(); it != nodes.end(); ++it){
+				pFrontier->insert(*it);
+			}
+			iterator++;
+		}
+	}
+
+	//Result
+	if(isSolution){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+list<node*> Problem::createListTreeNodes(list<Sucessor>* sucessors, node* pNode, int max_Depth, string strategy){
+	list<node*> nodes;
+	if(pNode->getDepth()+1 > max_Depth){
+		//Do nothing because we got to the maximum depth
+	}else{
+		for (std::list<Sucessor>::iterator it=sucessors->begin(); it != sucessors->end(); ++it){
+			int value = pNode->getValue()+1; //Placeholder
+			node* auxNode = new node(pNode, it->getState(), it->getCost(), it->getAction(), pNode->getDepth()+1, value);
+			nodes.push_back(auxNode);
+		}
+	}
+	return nodes;
+}
+
